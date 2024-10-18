1239ce7d20abbaa509f36de89b459e71ae6bcb69
Sen1Gao <sengao1024@outlook.com>
2024-10-18 22:07:49 +1100

    Add the function of converting labels from color to index.

---
+++ index_label_generator.py
@@ -0,0 +1,91 @@
+"""
+Group: Team1024
+File name: index_label_generator.py
+Author: Sen Gao
+"""
+
+import sys
+import argparse
+from pathlib import Path
+
+import numpy as np
+import cv2
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+
+import utilities
+
+def process_row(img_row:np.ndarray)->list:
+    index_row=[]
+    for value in img_row:
+        color_value_bgr=value.tolist()
+        color_value_rgb=tuple([color_value_bgr[2],color_value_bgr[1],color_value_bgr[0]])
+        index=utilities.index_lookup(color_value_rgb)
+        index_row.append(index)
+
+    return index_row
+
+def convert_image(color_label_path:Path,index_label_path:Path)->bool:
+    color_label=cv2.imread(color_label_path.as_posix(),cv2.IMREAD_UNCHANGED)
+    if color_label is None:
+        #print(f"Cannot load color label which is located in '{color_label_path.as_posix()}'!")
+        return False
+    
+    h=color_label.shape[0]
+    rows=[]
+    for row in range(h):
+        rows.append(color_label[row,:])
+    
+    with ThreadPoolExecutor() as executor:
+        result=list(executor.map(process_row,rows))
+    index_label=np.array(result,dtype=np.uint8)
+
+    #Save converted index label
+    if not cv2.imwrite(index_label_path.as_posix(),index_label):
+        #print(f"Cannot save index label which should be located in '{index_label_path.as_posix()}'!")
+        return False
+    
+    return True
+
+def main(args=None):
+    print("====================")
+    color_label_folder_path=Path(args.input_folder_path.as_posix())
+    print(f"Color label folder path: {color_label_folder_path.as_posix()}")
+
+    try:
+        print("Create a new folder to save generated index labels.")
+        index_label_folder_path=color_label_folder_path.parent/(color_label_folder_path.name+"_index")
+        index_label_folder_path.mkdir(exist_ok=False)
+
+        sub_color_folder_paths=[path for path in color_label_folder_path.iterdir() if path.is_dir()]
+        color_label_file_paths=[]
+        index_label_file_paths=[]
+        for sub_color_folder_path in sub_color_folder_paths:
+            sub_index_folder_path=index_label_folder_path/sub_color_folder_path.name
+            sub_index_folder_path.mkdir(exist_ok=False)
+            sub_color_folder_file_paths=[file for file in sub_color_folder_path.iterdir() if file.is_file() and file.suffix==".png"]
+            color_label_file_paths.extend(sub_color_folder_file_paths)
+            for sub_color_folder_file_path in sub_color_folder_file_paths:
+                index_label_file_paths.append(sub_index_folder_path/sub_color_folder_file_path.name)
+        print(f"Folder '{index_label_folder_path.as_posix()}' and its sub folder have been created successfully.")
+
+        print("Start generating index labels.")
+        for color_label_file_path,index_label_file_path in tqdm(zip(color_label_file_paths,index_label_file_paths),total=len(color_label_file_paths)):
+            convert_image(color_label_file_path,index_label_file_path)
+        print("Finish.")
+    except Exception as e:
+        print(e)
+        sys.exit(0)
+
+    
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--input_folder_path",
+                        type=Path,
+                        required=True,
+                        help="The path of folder of annotation. For example: --input_folder_path your/RUGD_annotations/folder/path")
+    args=parser.parse_args()
+    if not args.input_folder_path.exists() or not args.input_folder_path.is_dir():
+        print("The folder path inputted does not exist or it is not a folder path!")
+        sys.exit(0)
+    main(args)
---
+++ utilities.py
@@ -0,0 +1,71 @@
+"""
+Group: Team1024
+File name: index_label_generator.py
+Author: Sen Gao
+"""
+
+# RGB format
+index_color_mapping={0:(0,0,0),         # void
+                     1:(108,64,20),     # dirt
+                     2:(255,229,204),   # sand
+                     3:(0,102,0),       # grass
+                     4:(0,255,0),       # tree
+                     5:(0,153,153),     # ploe
+                     6:(0,128,255),     # water
+                     7:(0,0,255),       # sky
+                     8:(255,255,0),     # vehicle
+                     9:(255,0,127),     # container/generic-object
+                     10:(64,64,64),     # asphalt
+                     11:(255,128,0),    # gravel
+                     12:(255,0,0),      # building
+                     13:(153,76,0),     # mulch
+                     14:(102,102,0),    # rock-bed
+                     15:(102,0,0),      # log
+                     16:(0,255,128),    # bicycle
+                     17:(204,153,255),  # person
+                     18:(102,0,204),    # fence
+                     19:(255,153,204),  # bush
+                     20:(0,102,102),    # sign
+                     21:(153,204,255),  # rock
+                     22:(102,255,255),  # bridge
+                     23:(101,101,11),   # concrete
+                     24:(114,85,47)     # picnic-table
+                     }
+
+# RGB format
+color_index_mapping={(0,0,0):0,         # void
+                     (108,64,20):1,     # dirt
+                     (255,229,204):2,   # sand
+                     (0,102,0):3,       # grass
+                     (0,255,0):4,       # tree
+                     (0,153,153):5,     # ploe
+                     (0,128,255):6,     # water
+                     (0,0,255):7,       # sky
+                     (255,255,0):8,     # vehicle
+                     (255,0,127):9,     # container/generic-object
+                     (64,64,64):10,     # asphalt
+                     (255,128,0):11,    # gravel
+                     (255,0,0):12,      # building
+                     (153,76,0):13,     # mulch
+                     (102,102,0):14,    # rock-bed
+                     (102,0,0):15,      # log
+                     (0,255,128):16,    # bicycle
+                     (204,153,255):17,  # person
+                     (102,0,204):18,    # fence
+                     (255,153,204):19,  # bush
+                     (0,102,102):20,    # sign
+                     (153,204,255):21,  # rock
+                     (102,255,255):22,  # bridge
+                     (101,101,11):23,   # concrete
+                     (114,85,47):24     # picnic-table
+                     }
+
+def index_lookup(color)->int:
+    """
+    Get index of color from color_index_mapping where the format of color is RGB.\n
+    Therefore, you must convert color format to RGB before you pass variable 'color'\n
+    The variable 'color' is a tuple.
+    """
+    return color_index_mapping[color]
+        
+    
